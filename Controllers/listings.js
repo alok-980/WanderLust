d7b8fc29350f7c6a563c75e61e83cb0d57e4f900
Alok Chauhan <alokchuhan7896@gmail.com>
2025-03-13 12:46:40 +0530

    adding the functionalities of mapbox

--- Controllers/listings.js
+++ Controllers/listings.js
@@ -1,4 +1,8 @@
 const Listing = require("../models/listing");
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const mapToken = process.env.MAP_TOKEN;
+
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
 module.exports.index = async (req, res) => {
     let allListing = await Listing.find();
@@ -29,6 +33,11 @@ module.exports.showListingDetails = async (req, res) => {
 }
 
 module.exports.createListing = async (req, res) => {
+    let response = await geocodingClient.forwardGeocode({
+        query: req.body.location,
+        limit: 1
+    }).send()
+
     let url = req.file.path;
     let filename = req.file.filename;
 
@@ -45,6 +54,9 @@ module.exports.createListing = async (req, res) => {
     })
     newListing.owner = req.user._id;
     newListing.image = { url, filename };
+    
+    newListing.geometry = response.body.features[0].geometry;
+
     await newListing.save();
     req.flash("success", "New Listing Created!");
     res.redirect("/listing");
